e9b2a973390f72cf570b35931b5dcf4078e483df
Pratik Ranjan <pratikranjan4597@gmail.com>
2022-07-14 23:21:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,16 @@ var systemGuess = Math.floor(Math.random()*100)+1
 var guess=0
 function guessNumber()
 {
-    var num = document.getElementById('in').ariaValueMaxif
-    if(num == '' || num>100 || num<1)
+    var num = document.getElementById('in').value
+    if(num.length === 0)
     {
-        console.log('Input is not valid');
+        document.getElementById("out").innerText = "Please provide a number"
+    }
+    else if (num>100) {
+        document.getElementById("out").innerText = "Provide a number smaller than 100"
+    }
+    else if (num<1) {
+        document.getElementById("out").innerText = "Provide a number greater than 0"
     }
     else
     {
@@ -13,18 +19,18 @@ function guessNumber()
         var num = parseInt(document.getElementById('in').value)
         if(num === systemGuess)
         {
-            // console.log('Got the answer correctly' + guess);
+            document.getElementById('guess').innerText = ""
             document.getElementById('out').innerHTML = '<i>Got the answer correctly</i><br>You made '+guess+' guesses'
         }
         else if(num > systemGuess)
         {
-            // console.log('Guess a smaller number');
+            document.getElementById('guess').innerText = "Guesses Made: "+guess
             document.getElementById('out').innerText = 'Guess a smaller number'
         }
         else
         {
-            // console.log('Guess a greater number');
+            document.getElementById('guess').innerText = "Guesses Made: "+guess
             document.getElementById('out').innerText = 'Guess a greater number'
         }
     }
-}   
+} 
